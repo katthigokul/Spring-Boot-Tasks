9d761170fbdc65332f41795c214c16b9efb62422
katthigokul <gokulkatthi@gmail.com>
2019-08-04 19:47:26 +0530

    Added Property Resources

--- src/main/java/com/stackroute/dataseeder/ApplicationListenerSeedData.java
+++ src/main/java/com/stackroute/dataseeder/ApplicationListenerSeedData.java
@@ -3,12 +3,16 @@ package com.stackroute.dataseeder;
 import com.stackroute.domain.Track;
 import com.stackroute.repository.TrackRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 
 @Component
+@PropertySource("classpath:application.properties")
 public class ApplicationListenerSeedData implements ApplicationListener<ContextRefreshedEvent> {
     private TrackRepository trackRepository;
 
@@ -17,11 +21,19 @@ public class ApplicationListenerSeedData implements ApplicationListener<ContextR
         this.trackRepository = trackRepository;
     }
 
+    /* Using Environment to get property  values*/
+
+    @Autowired
+    private Environment environment;
+
+    /* To Get  the Property Values*/
+    @Value("${id}")
+    int id;
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
         System.out.println("Context Event Received");
-        Track track = new Track(1, "Baby", "Baby Song");
+        /*Track track = new Track(1, "Baby", "Baby Song");
         trackRepository.save(track);
         Track track2 = new Track(2, "Love me", "Love Song");
         trackRepository.save(track2);
@@ -32,7 +44,12 @@ public class ApplicationListenerSeedData implements ApplicationListener<ContextR
         Track track5 = new Track(5, "Love me Again", "Sad Song");
         trackRepository.save(track5);
 
+    }*/
+        try {
+            trackRepository.save(new Track(id, environment.getProperty("name"),environment.getProperty("comments")));
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+
     }
-
 }
-
