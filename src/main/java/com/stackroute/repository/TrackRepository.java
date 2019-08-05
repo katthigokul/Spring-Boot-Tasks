d268fd6cc69d524730f18a623d3877a2f4ac314a
katthigokul <gokulkatthi@gmail.com>
2019-08-05 16:58:57 +0530

    Addfshgs

---
+++ src/main/java/com/stackroute/repository/TrackRepository.java
@@ -0,0 +1,15 @@
+package com.stackroute.repository;
+
+import com.stackroute.domain.Track;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+//Adding Query By using @Query Annotations
+public interface TrackRepository extends JpaRepository<Track, Integer> {
+    @Query("Select t from Track t where t.name like ?1")
+    List<Track> searchTrackByName(String name);
+
+}
---
+++ src/main/java/com/stackroute/service/TrackService.java
@@ -0,0 +1,23 @@
+package com.stackroute.service;
+
+import com.stackroute.domain.Track;
+import com.stackroute.exception.TrackAlreadyExistsException;
+import com.stackroute.exception.TrackNotFoundException;
+
+import java.util.List;
+
+public interface TrackService {
+    public Track saveTrack(Track track) throws TrackAlreadyExistsException;
+
+    Track getTrackById(int id) throws TrackNotFoundException;
+
+    public List<Track> getAllTrack();
+
+    public List<Track> deleteTrackById(int id) throws TrackNotFoundException;
+
+    public Track updateTrackById(int id, Track updatedTrack) throws TrackNotFoundException;
+
+    //Searching Tracks By Name
+
+    public List<Track> searchTrackByName(String name) throws TrackNotFoundException;
+}
